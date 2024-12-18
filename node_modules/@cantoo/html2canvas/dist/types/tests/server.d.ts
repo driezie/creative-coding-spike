638c1b177d87606603aec61f9f9e0d70e50ed7ce
Jelte (aka Driezie) <80174866+driezie@users.noreply.github.com>
2024-12-18 14:08:44 +0100

    remove node modules

--- node_modules/@cantoo/html2canvas/dist/types/tests/server.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const app: import("express-serve-static-core").Express;
-export declare const corsApp: import("express-serve-static-core").Express;
-export declare const screenshotApp: import("express-serve-static-core").Express;
